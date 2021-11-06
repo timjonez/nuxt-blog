3e52b1f1dc3231d5b9022b40221411fe60dca903
Tim Jones <timjonez@protonmail.com>
2021-11-05 20:50:16 -0500

    [layouts] add default layout with header

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <header class="hero">
+      <h1 class="text-white text-2xl font-bold">Tim Jones</h1>
+      <ul class="flex justify-between text-white menu">
+        <li>BLOG</li>
+        <li>PORTFOLIO</li>
+        <li>GET IN TOUCH</li>
+      </ul>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.hero {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 15rem;
+  background-image: url(../static/code.jpg);
+  background-color: rgba(49, 49, 49, 0.8);
+  background-blend-mode: multiply;
+  padding: 1rem 1rem;
+}
+
+ul.menu li {
+  padding: 0rem 1rem;
+}
+
+</style>
